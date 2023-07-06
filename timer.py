356653c5accad340da15fe38cbe7e9ab5b0fdc80
Runaway2 <87344120+Runaway2@users.noreply.github.com>
2023-07-06 16:49:11 -0700

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,29 @@
+# import the time & playsound module
+# import the time & playsound module
+import time
+from playsound import playsound 
+  
+# define the countdown timer function
+def countdown_timer(seconds):
+    
+    while seconds > 0:       
+
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+
+        timer = f'{mins}:{secs}'
+
+        print(timer)
+        
+        seconds -= 1
+      
+    print('Time Up!')
+
+    playsound('/Users/kovidjaddu/Downloads/kovid-python-main/Mix.wav')
+  
+  
+# input time in seconds
+seconds = input("Enter the time in number of seconds: ")
+  
+# function call
+countdown_timer(int(seconds))
